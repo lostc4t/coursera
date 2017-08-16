43cdc825ab123396f6825def40f48be84f07faaf
Julie Jones <julie.jones@defensestorm.com>
2017-08-16 10:37:25 -0700

    totalValue issue

--- app.js
+++ app.js
@@ -5,12 +5,21 @@
   // module(name, array of dependencies)
   // controller will define the 'view' of our view model (index.html)
   angular.module('myFirstApp',[])
+
   .controller('myFirstController', function($scope){
-    $scope.name = 'Yaakov';
+    $scope.name = "Yaakov";
     $scope.sayHello = function() {
       return "Hello Coursera!";
     };
   });
+
+  angular.module('NameCalculator',[])
+  
+  .controller('NameCalculatorController', function($scope) {
+
+    $scope.name="";
+    $scope.totalValue = 0;
+  })
 
 
 })();  // IIFE  Immediately Invoked Function Expression - prevents variables from bleeding into global scope
