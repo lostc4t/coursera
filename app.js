39b8734eaa4701c79ea0ed81ef470a66060e1fa6
Julie Jones <julie.jones@defensestorm.com>
2017-08-16 09:59:17 -0700

    failed to instantiate app

---
+++ app.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  var x = 'hello';
+  // module(name, array of dependencies)
+  // controller will define the 'view' of our view model (index.html)
+  angular.module('myFirstApp',[])
+  .controller('myFirstController', function(){
+
+  })
+
+
+})();  // IIFE  Immediately Invoked Function Expression - prevents variables from bleeding into global scope
