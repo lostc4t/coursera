8c2252bc5f15d8770a84ae4b70e6308819422d06
Julie Jones <julie.jones@defensestorm.com>
2017-08-16 10:22:08 -0700

    blank screen

--- app.js
+++ app.js
@@ -5,9 +5,12 @@
   // module(name, array of dependencies)
   // controller will define the 'view' of our view model (index.html)
   angular.module('myFirstApp',[])
-  .controller('myFirstController', function(){
-
-  })
+  .controller('myFirstController', function($scope){
+    $scope.name = 'Yaakov';
+    $scope.sayHello = function() {
+      return "Hello Coursera!";
+    };
+  });
 
 
 })();  // IIFE  Immediately Invoked Function Expression - prevents variables from bleeding into global scope
